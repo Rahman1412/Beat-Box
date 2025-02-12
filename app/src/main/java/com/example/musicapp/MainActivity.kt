a7d7bc9be294a10c88c863cfe7e6fb4d43100430
rahman1412 <mohafizurrahman95@gmail.com>
2025-02-12 16:22:19 +0530

    Added Lifecycle Observer

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.musicapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,15 +14,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.musicapp.screens.MyApp
+import com.example.musicapp.services.MusicService
 import com.example.musicapp.ui.theme.MusicAppTheme
+import com.example.musicapp.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val vm : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        lifecycle.addObserver(vm)
         setContent {
             MusicAppTheme {
                 MyApp()
---
+++ app/src/main/java/com/example/musicapp/viewmodel/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.musicapp.viewmodel
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import com.example.musicapp.services.MusicService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    @ApplicationContext private val context: Context
+): ViewModel(), LifecycleEventObserver{
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when(event){
+            Lifecycle.Event.ON_CREATE -> Log.d("Created","Activity Created")
+            Lifecycle.Event.ON_START -> Log.d("Start","Activity Started")
+            Lifecycle.Event.ON_RESUME -> Log.d("Resume","Activity Resume")
+            Lifecycle.Event.ON_PAUSE -> Log.d("Pause","Activity Pause")
+            Lifecycle.Event.ON_STOP -> {
+                val intent = Intent(context, MusicService::class.java)
+                context.stopService(intent)
+            }
+            Lifecycle.Event.ON_DESTROY -> Log.d("Destroyed","Activity Destroyed")
+            Lifecycle.Event.ON_ANY -> Log.d("ANY","Activity ANY")
+        }
+    }
+}
